69c617553a479572985623cf268dd93856a63bec
NickdevC <nick_coles@msn.com>
2023-02-06 16:52:33 +0000

    Add view to render index.html

--- booking_app/views.py
+++ booking_app/views.py
@@ -3,6 +3,12 @@ from django.views import generic
 from .models import Appointment
 
 
+# Render index.html
+def index(request):
+    """ Return homepage """
+    return render(request, 'index.html')
+
+
 class ViewAppointments(generic.ListView):
     model = Appointment
     queryset = Appointment.objects.order_by('-date')
