2a80903bce2f6abee0286c47560fd4eea75430f4
NickdevC <nick_coles@msn.com>
2023-02-07 13:49:15 +0000

    Add appointments.view and render template

--- booking_app/urls.py
+++ booking_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('appointments/', views.AppointmentList.as_view(), name='appointments'),
 ]
--- booking_app/views.py
+++ booking_app/views.py
@@ -9,6 +9,24 @@ def index(request):
     return render(request, 'index.html')
 
 
+# Render appointments.html
+class AppointmentList(generic.ListView):
+    """
+    Render appointment list from the database
+    """
+    model = Appointment
+    template_name = 'appointments.html'
+    context_object_name = 'appointment_items'
+
+    def get_queryset(self):
+
+        queryset = {
+            'appointment_details': Appointment.objects.all()
+        }
+        return queryset
+
+
+# Render appointment cards in appointments.html
 class ViewAppointments(generic.ListView):
     model = Appointment
     queryset = Appointment.objects.order_by('-date')
