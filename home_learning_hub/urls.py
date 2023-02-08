4d6ccbf52e4837855e8262f41680552727863e8c
NickdevC <nick_coles@msn.com>
2023-02-08 16:44:28 +0000

    Install django-allauth

--- home_learning_hub/urls.py
+++ home_learning_hub/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path("", include('booking_app.urls'), name="booking-urls"),
+    path('accounts/', include('allauth.urls')),
 ]
