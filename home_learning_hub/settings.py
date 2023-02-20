c5ce2fe77d06e4504ccd152715911f294b1bc7b5
NickdevC <nick_coles@msn.com>
2023-02-20 14:01:35 +0000

    Update DEBUG value to allow True only in Gitpod

--- home_learning_hub/settings.py
+++ home_learning_hub/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = ['nickdevc-home-learning-hub.herokuapp.com', 'localhost']
 
